4ca8f6b39950c48357ce41558b47dfbae3b30f58
k-dash6 <dakozlova_1@edu.hse.ru>
2023-02-26 21:45:48 +0300

    fix locators

--- features/ui/locators/locators.py
+++ features/ui/locators/locators.py
@@ -1,23 +1,25 @@
 """ This file contains locators """
 from selenium.webdriver.common.by import By
 
-# Requests buttons
-LIST_USERS = (By.XPATH, "//a[contains(text(),'List users')]")
-SINGLE_USER = (By.XPATH, "//a[contains(text(),'Single user')]")
-SINGLE_USER_NOT_FOUND = (By.XPATH, "//a[contains(text(),'Single user not found')]")
-LIST_RESOURCE = (By.XPATH, "//a[contains(text(),'List <resource>')]")
-SINGLE_RESOURCE = (By.XPATH, "//a[contains(text(),'Single <resource>')]")
-SINGLE_RESOURCE_NOT_FOUND = (By.XPATH, "//a[contains(text(),'Single <resource> not found')]")
-CREATE = (By.XPATH, "//a[contains(text(),'Create')]")
-UPDATE_PUT = (By.XPATH, "//li[@data-id='put']/a[contains(text(),'Update')]")
-UPDATE_PATCH = (By.XPATH, "//li[@data-id='patch']/a[contains(text(),'Update')]")
-DELETE = (By.XPATH, "//a[contains(text(),'Delete')]")
-REGISTER_SUCCESSFUL = (By.XPATH, "//a[contains(text(),'Register - successful')]")
-REGISTER_UNSUCCESSFUL = (By.XPATH, "//a[contains(text(),'Register - unsuccessful')]")
-LOGIN_SUCCESSFUL = (By.XPATH, "//a[contains(text(),'Login - successful')]")
-LOGIN_UNSUCCESSFUL = (By.XPATH, "//a[contains(text(),'Login - unsuccessful')]")
-DELAY = (By.XPATH, "//a[contains(text(),'Delayed response')]")
 
-# Panels
-OUTPUT_RESPONSE = (By.XPATH, "//pre[@data-key='output-response']")
-RESPONSE_CODE = (By.CLASS_NAME, "response-code")
+class BaseLocators:
+    # Requests buttons
+    LIST_USERS = (By.XPATH, "//a[contains(text(),'List users')]")
+    SINGLE_USER = (By.XPATH, "//a[contains(text(),'Single user')]")
+    SINGLE_USER_NOT_FOUND = (By.XPATH, "//a[contains(text(),'Single user not found')]")
+    LIST_RESOURCE = (By.XPATH, "//a[contains(text(),'List <resource>')]")
+    SINGLE_RESOURCE = (By.XPATH, "//a[contains(text(),'Single <resource>')]")
+    SINGLE_RESOURCE_NOT_FOUND = (By.XPATH, "//a[contains(text(),'Single <resource> not found')]")
+    CREATE = (By.XPATH, "//a[contains(text(),'Create')]")
+    UPDATE_PUT = (By.XPATH, "//li[@data-id='put']/a[contains(text(),'Update')]")
+    UPDATE_PATCH = (By.XPATH, "//li[@data-id='patch']/a[contains(text(),'Update')]")
+    DELETE = (By.XPATH, "//a[contains(text(),'Delete')]")
+    REGISTER_SUCCESSFUL = (By.XPATH, "//a[contains(text(),'Register - successful')]")
+    REGISTER_UNSUCCESSFUL = (By.XPATH, "//a[contains(text(),'Register - unsuccessful')]")
+    LOGIN_SUCCESSFUL = (By.XPATH, "//a[contains(text(),'Login - successful')]")
+    LOGIN_UNSUCCESSFUL = (By.XPATH, "//a[contains(text(),'Login - unsuccessful')]")
+    DELAY = (By.XPATH, "//a[contains(text(),'Delayed response')]")
+
+    # Panels
+    OUTPUT_RESPONSE = (By.XPATH, "//pre[@data-key='output-response']")
+    RESPONSE_CODE = (By.CLASS_NAME, "response-code")
--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -6,18 +6,16 @@ from data_for_tests.data_for_requests import REGISTER_DATA_RIGHT, REGISTER_DATA_
     LOGIN_DATA_WRONG, UPDATE_CREATE_DATA
 from features.api.urls.urls import MainUrls
 from features.ui.apps.element_actions import click_to_element, get_text_from_element
-from features.ui.locators.locators import LIST_USERS, RESPONSE_CODE, OUTPUT_RESPONSE, DELAY, LOGIN_UNSUCCESSFUL, \
-    LOGIN_SUCCESSFUL, REGISTER_UNSUCCESSFUL, REGISTER_SUCCESSFUL, DELETE, UPDATE_PATCH, UPDATE_PUT, CREATE, \
-    SINGLE_RESOURCE_NOT_FOUND, SINGLE_RESOURCE, LIST_RESOURCE, SINGLE_USER_NOT_FOUND, SINGLE_USER
+from features.ui.locators.locators import BaseLocators
 
 
 def test_ui_list_users(driver_fixture):
     """ This test checks ui response matches api response for list users request """
-    click_to_element(LIST_USERS, driver_fixture)
+    click_to_element(BaseLocators.LIST_USERS, driver_fixture)
     response = requests.get(MainUrls.LIST_USERS)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -27,11 +25,11 @@ def test_ui_list_users(driver_fixture):
 
 def test_ui_single_user(driver_fixture):
     """ This test checks ui response matches api response for single user request """
-    click_to_element(SINGLE_USER, driver_fixture)
+    click_to_element(BaseLocators.SINGLE_USER, driver_fixture)
     response = requests.get(MainUrls.SINGLE_USER)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -41,11 +39,11 @@ def test_ui_single_user(driver_fixture):
 
 def test_ui_single_user_not_found(driver_fixture):
     """ This test checks ui response matches api response for single user not found request """
-    click_to_element(SINGLE_USER_NOT_FOUND, driver_fixture)
+    click_to_element(BaseLocators.SINGLE_USER_NOT_FOUND, driver_fixture)
     response = requests.get(MainUrls.SINGLE_USER_NOT_FOUND)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -55,11 +53,11 @@ def test_ui_single_user_not_found(driver_fixture):
 
 def test_ui_list_resource(driver_fixture):
     """ This test checks ui response matches api response for list resource request """
-    click_to_element(LIST_RESOURCE, driver_fixture)
+    click_to_element(BaseLocators.LIST_RESOURCE, driver_fixture)
     response = requests.get(MainUrls.LIST_RESOURCE)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -69,11 +67,11 @@ def test_ui_list_resource(driver_fixture):
 
 def test_ui_single_resource(driver_fixture):
     """ This test checks ui response matches api response for single resource request """
-    click_to_element(SINGLE_RESOURCE, driver_fixture)
+    click_to_element(BaseLocators.SINGLE_RESOURCE, driver_fixture)
     response = requests.get(MainUrls.SINGLE_RESOURCE)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -83,11 +81,11 @@ def test_ui_single_resource(driver_fixture):
 
 def test_ui_single_resource_not_found(driver_fixture):
     """ This test checks ui response matches api response for single resource not found request """
-    click_to_element(SINGLE_RESOURCE_NOT_FOUND, driver_fixture)
+    click_to_element(BaseLocators.SINGLE_RESOURCE_NOT_FOUND, driver_fixture)
     response = requests.get(MainUrls.SINGLE_RESOURCE_NOT_FOUND)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -97,10 +95,10 @@ def test_ui_single_resource_not_found(driver_fixture):
 
 def test_ui_create_user(driver_fixture):
     """ This test checks ui response codes matches api response for create user put request """
-    click_to_element(CREATE, driver_fixture)
+    click_to_element(BaseLocators.CREATE, driver_fixture)
     response = requests.post(MainUrls.CREATE_USER, data=UPDATE_CREATE_DATA)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -108,10 +106,10 @@ def test_ui_create_user(driver_fixture):
 
 def test_ui_update_user_put(driver_fixture, update_user_data):
     """ This test checks ui response codes matches api response for update user put request """
-    click_to_element(UPDATE_PUT, driver_fixture)
+    click_to_element(BaseLocators.UPDATE_PUT, driver_fixture)
     response = requests.put(MainUrls.EDIT_USER, data=update_user_data)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -119,10 +117,10 @@ def test_ui_update_user_put(driver_fixture, update_user_data):
 
 def test_ui_update_user_patch(driver_fixture, update_user_data):
     """ This test checks ui response codes matches api response for update user patch request """
-    click_to_element(UPDATE_PATCH, driver_fixture)
+    click_to_element(BaseLocators.UPDATE_PATCH, driver_fixture)
     response = requests.patch(MainUrls.EDIT_USER, data=update_user_data)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -130,10 +128,10 @@ def test_ui_update_user_patch(driver_fixture, update_user_data):
 
 def test_ui_delete_user(driver_fixture):
     """ This test checks ui response codes matches api response for delete user request """
-    click_to_element(DELETE, driver_fixture)
+    click_to_element(BaseLocators.DELETE, driver_fixture)
     response = requests.delete(MainUrls.EDIT_USER)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -141,11 +139,11 @@ def test_ui_delete_user(driver_fixture):
 
 def test_ui_register_successful(driver_fixture):
     """ This test checks ui response matches api response for successful register request """
-    click_to_element(REGISTER_SUCCESSFUL, driver_fixture)
+    click_to_element(BaseLocators.REGISTER_SUCCESSFUL, driver_fixture)
     response = requests.post(MainUrls.REGISTER, data=REGISTER_DATA_RIGHT)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -155,11 +153,11 @@ def test_ui_register_successful(driver_fixture):
 
 def test_ui_register_unsuccessful(driver_fixture):
     """ This test checks ui response matches api response for unsuccessful register request """
-    click_to_element(REGISTER_UNSUCCESSFUL, driver_fixture)
+    click_to_element(BaseLocators.REGISTER_UNSUCCESSFUL, driver_fixture)
     response = requests.post(MainUrls.REGISTER, data=REGISTER_DATA_WRONG)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -169,11 +167,11 @@ def test_ui_register_unsuccessful(driver_fixture):
 
 def test_ui_login_successful(driver_fixture):
     """ This test checks ui response matches api response for successful login request """
-    click_to_element(LOGIN_SUCCESSFUL, driver_fixture)
+    click_to_element(BaseLocators.LOGIN_SUCCESSFUL, driver_fixture)
     response = requests.post(MainUrls.LOGIN, data=LOGIN_DATA_RIGHT)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -183,11 +181,11 @@ def test_ui_login_successful(driver_fixture):
 
 def test_ui_login_unsuccessful(driver_fixture):
     """ This test checks ui response matches api response for unsuccessful login request """
-    click_to_element(LOGIN_UNSUCCESSFUL, driver_fixture)
+    click_to_element(BaseLocators.LOGIN_UNSUCCESSFUL, driver_fixture)
     response = requests.post(MainUrls.LOGIN, data=LOGIN_DATA_WRONG)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
@@ -197,11 +195,11 @@ def test_ui_login_unsuccessful(driver_fixture):
 
 def test_ui_delayed_response(driver_fixture):
     """ This test checks ui response matches api response for delayed request """
-    click_to_element(DELAY, driver_fixture)
+    click_to_element(BaseLocators.DELAY, driver_fixture)
     response = requests.get(MainUrls.DELAY)
 
-    code_ui = int(get_text_from_element(RESPONSE_CODE, driver_fixture))
-    response_output_ui = json.loads(get_text_from_element(OUTPUT_RESPONSE, driver_fixture))
+    code_ui = int(get_text_from_element(BaseLocators.RESPONSE_CODE, driver_fixture))
+    response_output_ui = json.loads(get_text_from_element(BaseLocators.OUTPUT_RESPONSE, driver_fixture))
 
     assert response.status_code == code_ui, f"Code values from api and ui are not matches. " \
                                             f"UI: {code_ui}, API: {response.status_code}"
